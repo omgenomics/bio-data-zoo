832abfae58e8355d17f03cca1cd0b7787b4d3b5d
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2024-05-31 11:31:50 -0700

    Add more BAM situations: not sorted, no mapped data

--- src/generate_bam.sh
+++ src/generate_bam.sh
@@ -1,28 +1,47 @@
 #!/bin/bash
 
-. ./src/lib.sh || exit
+DIR_SRC="${BASH_SOURCE%/*}"
+source "$DIR_SRC/lib.sh" || exit
 
 # ==============================================================================
 # Good BAM files
 # ==============================================================================
 
 URL="https://42basepairs.com/download/s3/1000genomes/phase3/data/NA12878/alignment/NA12878.chrom11.ILLUMINA.bwa.CEU.low_coverage.20121211.bam"
-DIR_OUT=data/bam/good
+DIR_OUT=$DIR_SRC/../data/bam/good
+mkdir -p "$DIR_OUT"
 
 # ------------------------------------------------------------------------------
-# Small BAM file
+# Basic BAM file
 # ------------------------------------------------------------------------------
 
 log "Creating valid BAM file"
 samtools view -b -h "$URL" 11:82365011-82366010 > "$DIR_OUT/basic.bam"
-validate $(samtools quickcheck -vvv "$DIR_OUT/basic.bam" 2>&1 | grep "has good EOF block")
+validate "$(samtools quickcheck -vvv "$DIR_OUT/basic.bam" 2>&1 | grep "good EOF block")"
+
+# ------------------------------------------------------------------------------
+# Unsorted BAM file
+# ------------------------------------------------------------------------------
+
+log "Creating unsorted BAM file"
+samtools view -b -h "$URL" 11:128989445-128990444 11:82365011-82366010 > "$DIR_OUT/unsorted.bam"
+validate "$(samtools index "$DIR_OUT/unsorted.bam" 2>&1 | grep "Unsorted positions on sequence")"
+
+# ------------------------------------------------------------------------------
+# No mapped data in BAM
+# ------------------------------------------------------------------------------
+
+log "Creating BAM with no mapped reads"
+samtools reset "$DIR_OUT/basic.bam" -O BAM > "$DIR_OUT/no_mapped_reads.bam"
+validate "$(samtools quickcheck "$DIR_OUT/no_mapped_reads.bam" 2>&1 | grep "no targets in header")"
 
 
 # ==============================================================================
 # Bad BAM files
 # ==============================================================================
 
-DIR_OUT=data/bam/bad
+DIR_OUT=$DIR_SRC/../data/bam/bad
+mkdir -p "$DIR_OUT"
 
 # ------------------------------------------------------------------------------
 # Truncated BAM file
@@ -30,9 +49,7 @@ DIR_OUT=data/bam/bad
 
 log "Creating truncated BAM file"
 curl -s -H "Range: bytes=0-25000" "$URL" > "$DIR_OUT/truncated.bam"
-
-# Samtools outputs a warning about an absent EOF marker
-validate $(samtools view "$DIR_OUT/truncated.bam" 2>&1 | grep "EOF marker is absent. The input is probably truncated")
+validate "$(samtools view "$DIR_OUT/truncated.bam" 2>&1 | grep "EOF marker is absent. The input is probably truncated")"
 
 # ------------------------------------------------------------------------------
 # BAI index is older than the BAM file (might needs reindexing to reflect latest BAM file contents)
@@ -43,6 +60,4 @@ samtools view -b -h "$URL" 11:82365011-82366010 > "$DIR_OUT/bai_older_than_data.
 samtools index "$DIR_OUT/bai_older_than_data.bam"
 sleep 1  # wait before run touch to make sure the timestamp is different enough
 touch "$DIR_OUT/bai_older_than_data.bam"
-
-# When using the index, samtools outputs a warning
-validate $(samtools idxstats "$DIR_OUT/bai_older_than_data.bam" 2>&1 | grep "The index file is older than the data file")
+validate "$(samtools idxstats "$DIR_OUT/bai_older_than_data.bam" 2>&1 | grep "The index file is older than the data file")"
--- src/lib.sh
+++ src/lib.sh
@@ -1,10 +1,12 @@
+#!/bin/bash
+
 function log() {
     echo -n "$1... "
 }
 
 function validate() {
     if [[ "$1" != "" ]]; then
-        echo "ok";
+        echo "done";
     else
         echo "failed";
         exit
