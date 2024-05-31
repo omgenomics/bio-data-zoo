5dea6a95cc638fbf625b33c0e1ca73b6e0766ba9
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2024-05-31 12:18:39 -0700

    Add sample BED files

---
+++ src/generate_bed.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+DIR_SRC="${BASH_SOURCE%/*}"
+source "$DIR_SRC/lib.sh" || exit
+
+# ==============================================================================
+# Good BED files
+# ==============================================================================
+
+URL="https://42basepairs.com/download/s3/human-pangenomics/pangenomes/freeze/freeze1/minigraph/hprc-v1.0-minigraph-chm13.bb.bed.gz"
+DIR_OUT=$DIR_SRC/../data/bed/good
+mkdir -p "$DIR_OUT"
+
+# ------------------------------------------------------------------------------
+# Basic BED file
+# ------------------------------------------------------------------------------
+
+log "Creating valid BED file"
+curl -s "$URL" | zcat | head -n 10 > "$DIR_OUT/basic.bed"
+validate "ok"
+DIR_BASIC="$DIR_OUT/basic.bed"
+
+# ------------------------------------------------------------------------------
+# Unsorted BED file
+# ------------------------------------------------------------------------------
+
+log "Creating unsorted BED file"
+curl -s "$URL" | zcat | head -n 50 | tail -n 10 > "$DIR_OUT/unsorted.bed"
+cat "$DIR_BASIC" >> "$DIR_OUT/unsorted.bed"
+validate "$(diff "$DIR_OUT/unsorted.bed" <(bedtools sort -i "$DIR_OUT/unsorted.bed"))"
+
+
+# ==============================================================================
+# Bad BED files
+# ==============================================================================
+
+DIR_OUT=$DIR_SRC/../data/bed/bad
+mkdir -p "$DIR_OUT"
+
+# ------------------------------------------------------------------------------
+# BED file with spaces instead of tabs
+# ------------------------------------------------------------------------------
+
+log "Creating BED file using spaces instead of tabs"
+sed 's/\t/    /g' "$DIR_BASIC" > "$DIR_OUT/spaces.bed"
+validate "$(bedtools merge -i "$DIR_OUT/spaces.bed" 2>&1 | grep "unable to open file or unable to determine types")"
+
+# ------------------------------------------------------------------------------
+# BED file with invalid ranges
+# ------------------------------------------------------------------------------
+
+log "Creating BED file with negative coordinates"
+sed 's/3634/-3634/' "$DIR_BASIC" > "$DIR_OUT/negative_coords.bed"
+validate "$(bedtools merge -i "$DIR_OUT/negative_coords.bed" 2>&1 | grep "Invalid record in file")"
+
+log "Creating BED file with start > end coordinates"
+sed 's/3634/9999/' "$DIR_BASIC" > "$DIR_OUT/start_greater_than_end_coords.bed"
+validate "$(bedtools merge -i "$DIR_OUT/start_greater_than_end_coords.bed" 2>&1 | grep "unable to open file or unable to determine types")"
+
+log "Creating BED file with non-integer coordinates"
+sed 's/3634/3.63/' "$DIR_BASIC" > "$DIR_OUT/non_integer_coords.bed"
+validate "$(bedtools merge -i "$DIR_OUT/non_integer_coords.bed" 2>&1 | grep "unable to open file or unable to determine types")"
